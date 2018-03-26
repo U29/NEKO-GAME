dfc03da29ce05ae1e2d594da26af4f24ef008a9d
ユゥ <yuluthi@gmail.com>
2018-03-26 11:32:50 +0900

    増殖仕様
    増殖仕様で増殖しよう

--- main.py
+++ main.py
@@ -15,22 +15,26 @@ def main():
 
     backimg = pygame.image.load("bg.png").convert()
     playerimg = pygame.image.load("player.png").convert_alpha()
+    neko = 0
+
+    screen.blit(backimg, (0, 0))
 
     while 1:
         pygame.display.update()
         pygame.time.delay(30)
-        screen.fill((0, 0, 0))
-        screen.blit(backimg, (0, 0))
-        screen.blit(playerimg, (x, y))
-        text = font.render("NEKO", True, (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+        # screen.blit(playerimg, (x, y))
+        screen.blit(playerimg, (random.randint(0, w), random.randint(0, h)))
+        text = font.render("NEKO: " + str(neko), True, (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
         # pygame.draw.circle(screen, (255, 51, 51), (random.randint(0, h), random.randint(0, h)), 15, 0)
-        screen.blit(text, [10, 10])
+        neko += 1
+        # screen.blit(text, [10, 10])
+        print('NEKO: ' + str(neko))
 
         for event in pygame.event.get():
-            if event.type == MOUSEMOTION:
+            '''if event.type == MOUSEMOTION:
                 x, y = event.pos
                 x -= playerimg.get_width() / 2
-                y -= playerimg.get_height() / 2
+                y -= playerimg.get_height() / 2'''
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
